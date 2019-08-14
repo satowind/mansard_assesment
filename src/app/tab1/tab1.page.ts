ee6f86e13b80dc4282bf2b7a7036f8e1f5840cd0
satowind <evensatowind@gmail.com>
2019-08-14 21:58:49 +0100

    add assets and is good to go

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page {
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
 
   constructor() {}
 
+  slideOptsTop = {
+    spaceBetween: 0,
+    slidesPerView: 1.15
+  };
+  /*
+    slider with more narrow cards, 2 fully visible slides
+  */
+  slideOptsThumbs = {
+    spaceBetween: 0,
+    slidesPerView: 2.65
+  };
 }
